51cb19afc80d2af8786c5c03d3d6cc735c3644df
Mathias Ertl <mati@fsinf.at>
2010-10-12 16:42:01 +0000

    flatter dir-structure

---
+++ mod_authn_url.c
@@ -0,0 +1,282 @@
+/* This file is licensed under the Apache License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ *
+ *
+ */
+
+#include "apr_strings.h"
+
+#define APR_WANT_STRFUNC
+#include "apr_want.h"
+
+#include "ap_provider.h"
+#include "httpd.h"
+#include "http_config.h"
+#include "http_core.h"
+#include "http_log.h"
+#include "http_request.h"
+#include "http_protocol.h"
+
+#include "mod_auth.h"
+
+#include <curl/curl.h> /* muhaha */
+
+/* configuration parameters */
+
+#define APACHE_OLDER_THAN(major,minor) (AP_SERVER_MAJORVERSION_NUMBER < major) || (AP_SERVER_MAJORVERSION_NUMBER == major && AP_SERVER_MINORVERSION_NUMBER < minor)
+
+typedef struct {
+    char *url;
+    CURL *session;
+    int forward_ip;
+    char *group;
+} authn_url_config;
+
+
+static apr_status_t url_cleanup(void *data) {
+    authn_url_config *conf = (authn_url_config *)data;
+
+    if (conf->session) {
+        curl_easy_cleanup(conf->session);
+        conf->session = NULL;
+    }
+
+    return 0;
+}
+
+static void *create_authn_url_dir_config(apr_pool_t *p, char *d)
+{
+    authn_url_config *conf = apr_palloc(p, sizeof(*conf));
+    conf->url = NULL;
+    conf->session = NULL;
+    conf->forward_ip = 0;
+    conf->group = NULL;
+
+    /* register cleanup handler */
+    apr_pool_cleanup_register(p, conf, url_cleanup, NULL);
+
+    return conf;
+}
+
+static const char *url_set_locator(cmd_parms *cmd,
+                                   void *conf_data, const char *arg)
+{
+    if (!*arg || !ap_is_url(arg))
+        return "URL not specified";
+
+    /* init url */
+    authn_url_config *conf = (authn_url_config *)conf_data;
+    conf->url = apr_psprintf(cmd->pool, "%s%s", arg, 
+			     /* add trailing slash if omitted */
+			     (arg[strlen(arg)-1] == '/')?"":"/");
+
+    /* init CURL session if not already initialized */
+    if (!conf->session) {
+        conf->session = curl_easy_init();
+        if (!conf->session)
+            return "Could not initialize HTTP request library";
+    }
+    return NULL;
+}
+
+static const char *url_set_group(cmd_parms *cmd,
+                                   void *conf_data, const char *arg)
+{
+    if (!*arg)
+        return "Group not specified";
+
+    /* add group */
+    authn_url_config *conf = (authn_url_config *)conf_data;
+    conf->group = apr_pstrdup(cmd->pool, arg);
+
+    return NULL;
+}
+
+static const command_rec authn_url_cmds[] =
+{
+    /* for now, the one protocol implemented is:
+       - RestAuth-POST: POST AuthURL/users/<user>/ (with password=<password> as www-urlencoded POST data)
+                        GET AuthURL/groups/<group>/<user>/ to check if user is in a group
+     */
+    AP_INIT_ITERATE("URLAuthAddress", url_set_locator, NULL, OR_AUTHCFG,
+        "The URL of the authentication service"),
+    AP_INIT_ITERATE("URLAuthGroup", url_set_group, NULL, OR_AUTHCFG, /* TODO: maybe use Require (authz) */
+        "The group to be validated against"),
+    AP_INIT_FLAG("URLAuthForwardIP", ap_set_flag_slot,
+        (void *)APR_OFFSETOF(authn_url_config, forward_ip), OR_AUTHCFG,
+        "Limited to 'on' or 'off'"),
+    {NULL}
+};
+
+/* basic authentication handler */
+
+/* utility function */
+static char* url_pescape(apr_pool_t *p, const char *str)
+{
+    // allocate 3 times the size of str
+    char *escaped = apr_palloc(p, strlen(str)*3+1);
+    char *escaped_str = escaped;
+
+    while (*str) {
+        if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z') || (*str >= '0' && *str <= '9'))
+            *escaped_str = *str;
+        else if (*str == ' ')
+            *escaped_str = '+';
+        else {
+            *escaped_str = '%';
+            *(escaped_str+1) = ((*str/16) < 10) ? ('0' + *str/16) : ('A' - 10 + *str/16);
+            *(escaped_str+2) = ((*str%16) < 10) ? ('0' + *str%16) : ('A' - 10 + *str%16);
+            escaped_str += 2;
+        }
+
+        escaped_str++;
+        str++;
+    }
+    *escaped_str = '\0';
+
+    return escaped;
+}
+
+/* function to set the necessary curl options */
+static void config_curl_session(CURL *session, char *url, char *post_data) {
+  curl_easy_setopt(session, CURLOPT_NETRC, CURL_NETRC_IGNORED);
+  curl_easy_setopt(session, CURLOPT_NOSIGNAL, 1L);
+  curl_easy_setopt(session, CURLOPT_FAILONERROR, 1L);
+  
+  if (post_data) {
+    curl_easy_setopt(session, CURLOPT_POST, 1L);
+    curl_easy_setopt(session, CURLOPT_POSTFIELDS, post_data);
+  }
+
+  curl_easy_setopt(session, CURLOPT_URL, url);
+
+}
+
+module AP_MODULE_DECLARE_DATA authn_url_module;
+
+static authn_status check_url(request_rec *r, const char *user,
+                                    const char *sent_pw)
+{
+    authn_url_config *conf = ap_get_module_config(r->per_dir_config, &authn_url_module);
+    authn_status res = AUTH_USER_NOT_FOUND;
+
+    /* ignore if not configured */
+    if (!conf->url)
+        return AUTH_USER_NOT_FOUND;
+
+    /* create url and storage */
+    apr_pool_t *url_pool = NULL;
+    char *url;
+    apr_pool_create(&url_pool, r->pool);
+
+    /* fetch client ip */
+    char *ip = r->connection->remote_ip;
+
+    char *post_data = apr_psprintf(url_pool, "password=%s%s%s",
+				   url_pescape(url_pool, sent_pw),
+				   /* ip data if requested */
+				   (conf->forward_ip)?"&ip=":"",
+				   (conf->forward_ip)?url_pescape(url_pool, ip):"");
+
+    url	= apr_psprintf(url_pool, "%susers/%s/", conf->url,
+		       url_pescape(url_pool, user));
+
+    config_curl_session(conf->session, url, post_data);
+
+    int curl_status;
+    long curl_http_code;
+    /* get response code - 200 OK, 404 NOT OK */
+    curl_status = curl_easy_perform(conf->session);
+    curl_status += curl_easy_getinfo(conf->session, CURLINFO_RESPONSE_CODE, &curl_http_code);
+
+    /*ap_log_rerror(APLOG_MARK, APLOG_NOTICE, APR_SUCCESS, r,
+                      "REST auth URL request: %s", url);*/
+
+    curl_easy_reset(conf->session);
+	
+    /* if status is not 200, return */
+    if (curl_status != CURLE_OK || curl_http_code != 200) {
+    	apr_pool_destroy(url_pool);
+    	return AUTH_DENIED;
+    }
+
+    /* if no group is set, grant access */
+    if (!conf->group) {
+    	apr_pool_destroy(url_pool);
+	return AUTH_GRANTED;
+    }
+
+    /* user exists with valid password, now check if user is in group */
+    /* compile GET url/parameters */
+
+    url = apr_psprintf(url_pool, "%sgroups/%s/%s/", conf->url,
+                         url_pescape(url_pool, conf->group),
+                         url_pescape(url_pool, user));
+
+    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, APR_SUCCESS, r,
+		  "REST auth valid group check: %s", url);
+
+
+    /* set request parameters */
+    config_curl_session(conf->session, url, NULL);
+
+    /* get response code - 200 OK, 404 NOT OK */
+    curl_status = curl_easy_perform(conf->session);
+    curl_status += curl_easy_getinfo(conf->session, CURLINFO_RESPONSE_CODE, &curl_http_code);
+
+    apr_pool_destroy(url_pool);
+    curl_easy_reset(conf->session);
+
+    /* group exists, and user is in the specified group */
+    if (curl_status == CURLE_OK && curl_http_code == 200)
+    	return AUTH_GRANTED;
+    else
+        return AUTH_DENIED;
+}
+
+/* module stuff */
+
+static const authn_provider authn_url_provider =
+{
+    &check_url,
+    NULL
+};
+
+static void register_hooks(apr_pool_t *p)
+{
+#if APACHE_OLDER_THAN(2,3)
+    ap_register_provider(p, AUTHN_PROVIDER_GROUP, "url", "0",
+                         &authn_url_provider);
+#else
+    ap_register_auth_provider(p, AUTHN_PROVIDER_GROUP, "url",
+                              AUTHN_PROVIDER_VERSION,
+                              &authn_url_provider, AP_AUTH_INTERNAL_PER_CONF); 
+#endif
+}
+
+#if APACHE_OLDER_THAN(2,3)
+APLOG_USE_MODULE(authn_url);
+module AP_MODULE_DECLARE_DATA authn_url_module =
+#else
+AP_DECLARE_MODULE(authn_url) =
+#endif
+{
+    STANDARD20_MODULE_STUFF,
+    create_authn_url_dir_config,  /* create config structure per directory */
+    NULL,                         /* dir merger ensure strictness */
+    NULL,                         /* server config */
+    NULL,                         /* merge server config */
+    authn_url_cmds,               /* command apr_table_t */
+    register_hooks                /* register hooks */
+};
